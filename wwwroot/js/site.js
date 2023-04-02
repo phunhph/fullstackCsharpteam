3e0e517d01dd94d2620797e82f718a78efea9c83
Nguyen huu phu <phunhph33261@fpt.com.edu.vn>
2023-04-02 11:55:05 +0700

    02042023

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById("profile").onclick = function () {
+    document.querySelector(".main-profile").classList.add("show");
+}
