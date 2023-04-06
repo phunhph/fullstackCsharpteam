9838f3c31745bc8cedfe08befb4b119f3e57b277
phunhph <119302648+phunhph@users.noreply.github.com>
2023-04-06 21:11:28 +0700

    Update LoginDAO.cs

--- DAO/LoginDAO.cs
+++ DAO/LoginDAO.cs
@@ -6,7 +6,7 @@ using fullstackCsharp.Models;
 public class LoginDAO
 {
     static string connString = "Data Source=MSI\\MSSQLSERVER01;Initial Catalog=QuanLy;Integrated Security=True;TrustServerCertificate=True";
-    /* static string connString = "Server=tcp:employee-management.database.windows.net,1433;Initial Catalog=employee-management;Persist Security Info=False;User ID=quyethieuphu;Password=Employee-management;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";*/
+  
     public bool ValidateUser(Login Login)
     {
         bool result = false;
