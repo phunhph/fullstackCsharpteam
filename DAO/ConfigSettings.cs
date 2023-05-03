61c488d48391f03a47ed494b5f40a914a55b6ff1
Nguyen huu phu <phunhph33261@fpt.com.edu.vn>
2023-05-03 17:20:02 +0700

    len lai

---
+++ DAO/ConfigSettings.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace fullstackCsharp.DAO;
+public static class ConfigSettings
+{
+    public static string connString { get; }
+    static ConfigSettings()
+    {
+        var configurationBuilder = new ConfigurationBuilder();
+        string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+        configurationBuilder.AddJsonFile(path, false);
+        connString = configurationBuilder.Build().GetSection("ConnectionStrings:AzureDB").Value;
+    }
+}
