6ebe6c5217067aadf7458fb4d0bbf6131caae210
Nguyen huu phu <phunhph33261@fpt.com.edu.vn>
2023-05-20 21:05:38 +0700

    xong

---
+++ Models/refPass.cs
@@ -0,0 +1,20 @@
+﻿namespace fullstackCsharp.Models
+{
+    public class refPass
+    {
+        public string password { get; set; }
+        public string newpassword { get; set; }
+        public string key { get; set; }
+
+        public refPass() { }
+
+        public refPass(string password, string newpassword, string key) {
+
+            this.password = password;
+            this.newpassword = newpassword;
+            this.key = key;
+
+
+        }
+    }
+}
